796eacee6427e44f3c4307b0e55ce154c575fccf
Jason Reilly <jdr0887@renci.org>
2013-04-08 13:00:08 -0400

    saving attributes keyed off of prefix

--- ncgenes-depth-of-coverage-workflow/src/main/java/edu/unc/mapseq/pipeline/ncgenes/doc/NCGenesDOCPipeline.java
+++ ncgenes-depth-of-coverage-workflow/src/main/java/edu/unc/mapseq/pipeline/ncgenes/doc/NCGenesDOCPipeline.java
@@ -1,12 +1,20 @@
 package edu.unc.mapseq.pipeline.ncgenes.doc;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.Graph;
@@ -192,6 +200,7 @@ public class NCGenesDOCPipeline extends AbstractPipeline<NCGenesDOCPipelineBeanS
 
     @Override
     public void postRun() throws PipelineException {
+        logger.info("ENTERING postRun()");
 
         File mapseqrc = new File(System.getProperty("user.home"), ".mapseqrc");
         Executor executor = BashExecutor.getInstance();
@@ -230,15 +239,144 @@ public class NCGenesDOCPipeline extends AbstractPipeline<NCGenesDOCPipelineBeanS
 
             String prefix = null;
             Set<EntityAttribute> attributeSet = htsfSample.getAttributes();
-            Iterator<EntityAttribute> attributeIter = attributeSet.iterator();
-            while (attributeIter.hasNext()) {
-                EntityAttribute attribute = attributeIter.next();
-                if ("GATKDepthOfCoverage.prefix".equals(attribute.getName())) {
-                    prefix = attribute.getValue();
+            if (attributeSet == null) {
+                attributeSet = new HashSet<EntityAttribute>();
+            }
+            if (attributeSet.size() > 0) {
+                Iterator<EntityAttribute> attributeIter = attributeSet.iterator();
+                while (attributeIter.hasNext()) {
+                    EntityAttribute attribute = attributeIter.next();
+                    if ("GATKDepthOfCoverage.prefix".equals(attribute.getName())) {
+                        prefix = attribute.getValue();
+                    }
                 }
             }
 
             Set<FileData> fileDataSet = htsfSample.getFileDatas();
+
+            Set<String> entityAttributeNameSet = new HashSet<String>();
+
+            for (EntityAttribute attribute : attributeSet) {
+                entityAttributeNameSet.add(attribute.getName());
+            }
+
+            Set<String> synchSet = Collections.synchronizedSet(entityAttributeNameSet);
+
+            Collection<File> potentialFileList = FileUtils.listFiles(
+                    outputDirectory,
+                    FileFilterUtils.and(FileFilterUtils.prefixFileFilter(prefix),
+                            FileFilterUtils.suffixFileFilter(".coverage.sample_summary")), null);
+
+            if (potentialFileList != null && potentialFileList.size() > 0) {
+                File docFile = potentialFileList.iterator().next();
+                try {
+                    List<String> lines = FileUtils.readLines(docFile);
+                    if (lines != null) {
+                        Iterator<String> lineIter = lines.iterator();
+                        while (lineIter.hasNext()) {
+                            String line = lineIter.next();
+                            if (line.contains("Total")) {
+                                String[] split = StringUtils.split(line);
+
+                                String totalCoverageKey = String.format("GATKDepthOfCoverage.%s.totalCoverage", prefix);
+                                if (synchSet.contains(totalCoverageKey)) {
+                                    for (EntityAttribute attribute : attributeSet) {
+                                        if (attribute.getName().equals(totalCoverageKey)) {
+                                            attribute.setValue(split[1]);
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    attributeSet.add(new EntityAttribute(totalCoverageKey, split[1]));
+                                }
+
+                                String meanCoverageKey = String.format("GATKDepthOfCoverage.%s.mean", prefix);
+                                if (synchSet.contains(meanCoverageKey)) {
+                                    for (EntityAttribute attribute : attributeSet) {
+                                        if (attribute.getName().equals(meanCoverageKey)) {
+                                            attribute.setValue(split[1]);
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    attributeSet.add(new EntityAttribute(meanCoverageKey, split[2]));
+                                }
+                            }
+                        }
+                    }
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+
+            potentialFileList = FileUtils.listFiles(
+                    outputDirectory,
+                    FileFilterUtils.and(FileFilterUtils.prefixFileFilter(prefix),
+                            FileFilterUtils.suffixFileFilter(".coverage.sample_interval_summary")), null);
+
+            Long totalPassedReads = null;
+            for (EntityAttribute attribute : attributeSet) {
+                if ("SAMToolsFlagstat.totalPassedReads".equals(attribute.getName())) {
+                    totalPassedReads = Long.valueOf(attribute.getValue());
+                    break;
+                }
+            }
+
+            if (potentialFileList != null && potentialFileList.size() > 0) {
+                try {
+                    File docFile = potentialFileList.iterator().next();
+                    BufferedReader br = new BufferedReader(new FileReader(docFile));
+                    String line;
+                    long totalCoverageCount = 0;
+                    br.readLine();
+                    while ((line = br.readLine()) != null) {
+                        totalCoverageCount += Long.valueOf(StringUtils.split(line)[1].trim());
+                    }
+                    br.close();
+
+                    String totalCoverageCountKey = String.format(
+                            "GATKDepthOfCoverage.%s.sample_interval_summary.totalCoverageCount", prefix);
+
+                    if (synchSet.contains(totalCoverageCountKey)) {
+                        for (EntityAttribute attribute : attributeSet) {
+                            if (attribute.getName().equals(totalCoverageCountKey)) {
+                                attribute.setValue(totalCoverageCount + "");
+                                break;
+                            }
+                        }
+                    } else {
+                        attributeSet.add(new EntityAttribute(totalCoverageCountKey, totalCoverageCount + ""));
+                    }
+
+                    String numberOnTargetKey = String.format("%s.numberOnTarget", prefix);
+
+                    if (synchSet.contains(numberOnTargetKey)) {
+                        for (EntityAttribute attribute : attributeSet) {
+                            if (attribute.getName().equals(numberOnTargetKey) && totalPassedReads != null) {
+                                attribute.setValue((double) totalCoverageCount / (totalPassedReads * 100) + "");
+                                break;
+                            }
+                        }
+                    } else {
+                        attributeSet.add(new EntityAttribute(numberOnTargetKey, (double) totalCoverageCount
+                                / (totalPassedReads * 100) + ""));
+                    }
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                htsfSample.setAttributes(attributeSet);
+                this.pipelineBeanService.getMaPSeqDAOBean().getHTSFSampleDAO().save(htsfSample);
+            } catch (MaPSeqDAOException e) {
+                e.printStackTrace();
+            }
 
             File bamFile = null;
 
