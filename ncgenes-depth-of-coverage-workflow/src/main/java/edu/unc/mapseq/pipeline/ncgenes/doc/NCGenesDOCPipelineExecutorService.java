a1d4a1f90503bd71e1b1f8002395e363842534d8
Jason Reilly <jdr0887@renci.org>
2013-04-18 14:35:14 -0400

    fixing period

--- ncgenes-depth-of-coverage-workflow/src/main/java/edu/unc/mapseq/pipeline/ncgenes/doc/NCGenesDOCPipelineExecutorService.java
+++ ncgenes-depth-of-coverage-workflow/src/main/java/edu/unc/mapseq/pipeline/ncgenes/doc/NCGenesDOCPipelineExecutorService.java
@@ -22,7 +22,7 @@ public class NCGenesDOCPipelineExecutorService {
     public void start() throws Exception {
         logger.info("ENTERING start()");
         long delay = 1 * 60 * 1000; // 1 minute
-        mainTimer.scheduleAtFixedRate(task, delay, period);
+        mainTimer.scheduleAtFixedRate(task, delay, period * 60 * 1000);
     }
 
     public void stop() throws Exception {
