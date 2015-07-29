70e053185cbf8de07652c7aaa37c9ff515ff2abc
Jason Reilly <jdr0887@gmail.com>
2015-07-29 12:36:01 -0400

    changing path

--- ncgenes-depth-of-coverage-workflow/src/test/java/edu/unc/mapseq/workflow/NCGenesDOCWorkflowTest.java
+++ ncgenes-depth-of-coverage-workflow/src/test/java/edu/unc/mapseq/workflow/NCGenesDOCWorkflowTest.java
@@ -46,7 +46,7 @@ public class NCGenesDOCWorkflowTest {
 
         CondorDOTExporter<CondorJob, CondorJobEdge> dotExporter = new CondorDOTExporter<CondorJob, CondorJobEdge>(
                 vnpId, vnpLabel, null, null, null, null);
-        File srcSiteResourcesImagesDir = new File("src/site/resources/images");
+        File srcSiteResourcesImagesDir = new File("../src/site/resources/images");
         if (!srcSiteResourcesImagesDir.exists()) {
             srcSiteResourcesImagesDir.mkdirs();
         }
